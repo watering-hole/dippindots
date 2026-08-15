a808b8202141eeec561ba1bd37f77996d6bcec38
Devin <Devin.Ramsden@jhuapl.edu>
2026-08-15 18:06:24 -0400

    lualine update

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,11 @@
+local function git_diff_side()
+    if not vim.wo.diff then
+        return ""
+    end
+
+    return vim.fn.expand("%"):match("^fugitive://") and "HEAD" or "WORKTREE"
+end
+
 return {
   options = {
     icons_enabled = true,
@@ -14,13 +22,13 @@ return {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_c = {git_diff_side, 'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
   inactive_sections = {
-    lualine_c = {'filename'},
+    lualine_c = {git_diff_side, 'filename'},
     lualine_x = {'location'},
   },
 }
